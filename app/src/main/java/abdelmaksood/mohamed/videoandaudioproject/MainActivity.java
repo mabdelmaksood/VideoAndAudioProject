dcbb15d8abdfde7b61e61ca7c642403b1aa158ab
mohamed abdelmaksood <m.abdelmaksood93@gmail.com>
2019-10-04 22:27:14 +0200

    added seek bar for music

--- app/src/main/java/abdelmaksood/mohamed/videoandaudioproject/MainActivity.java
+++ app/src/main/java/abdelmaksood/mohamed/videoandaudioproject/MainActivity.java
@@ -11,19 +11,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.SeekBar;
-import android.widget.Toast;
+//import android.widget.Toast;
 import android.widget.VideoView;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnCompletionListener, SeekBar.OnSeekBarChangeListener {
-    VideoView video;
-    Button btnVideo;
-    Button btnAudio;
-    Button btnSilent;
-    MediaPlayer musicPlayer;
-    SeekBar musicBar;
-    AudioManager audioService;
-    boolean videoInit=false;
-
+    private VideoView video;
+    private Button btnVideo;
+    private Button btnAudio;
+    private Button btnSilent;
+    private MediaPlayer musicPlayer;
+    private SeekBar volumeBar;
+    private SeekBar positionBar;
+    private AudioManager audioService;
+    private boolean videoInit=false;
+    private Timer mytimer;
 
 
     MediaController myMediaCtrl;
@@ -37,10 +40,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnVideo=findViewById(R.id.btnVideo);
         btnAudio=findViewById(R.id.audiobtn);
         btnSilent=findViewById(R.id.audiobtn2);
-        musicBar=findViewById(R.id.audioSeeker);
+        volumeBar=findViewById(R.id.audioSeeker);
+        positionBar=findViewById(R.id.musicSeeker);
         audioService= (AudioManager)  getSystemService(AUDIO_SERVICE);
         int maxVolume=audioService.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         int currVolume=audioService.getStreamVolume(AudioManager.STREAM_MUSIC);
+
 
         //set on click listener
         btnSilent.setOnClickListener(MainActivity.this);
@@ -60,9 +65,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         video.setVideoURI(videoUri);
         */
 
-        musicBar.setMax(maxVolume);
-        musicBar.setProgress(currVolume);
-        musicBar.setOnSeekBarChangeListener(MainActivity.this);
+        volumeBar.setMax(maxVolume);
+        volumeBar.setProgress(currVolume);
+        volumeBar.setOnSeekBarChangeListener(MainActivity.this);
+        positionBar.setOnSeekBarChangeListener(this);
+        positionBar.setMax(musicPlayer.getDuration());
 
 
 
@@ -77,10 +84,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     video.setVideoPath(videoPath);
                     video.setMediaController(myMediaCtrl);
                     myMediaCtrl.setAnchorView(video);
+                    mytimer=new Timer();
+                    mytimer.scheduleAtFixedRate(new TimerTask() {
+                        @Override
+                        public void run() {
+                            positionBar.setProgress(musicPlayer.getCurrentPosition());
+                        }
+                    },0,1000);
                 }
                 if(video.isPlaying()){
                     video.pause();
                     musicPlayer.pause();
+                    mytimer.cancel();
                 }
                 else if (is_start) {
 
@@ -99,6 +114,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 btnAudio.animate().alpha(0);
                 btnSilent.animate().alpha(1);
                 musicPlayer.pause();
+                mytimer.cancel();
                 break;
             case R.id.audiobtn2:
                 btnAudio.setClickable(true);
@@ -118,19 +134,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         if(fromUser){
-            //Toast.makeText(getApplicationContext(),progress+"",Toast.LENGTH_SHORT).show();
-            audioService.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+            switch(seekBar.getId()){
+                case R.id.audioSeeker:
+                    audioService.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+                    break;
+                case R.id.musicSeeker:
+                    //Toast.makeText(getApplicationContext(),progress+"",Toast.LENGTH_SHORT).show();
+                    musicPlayer.seekTo(positionBar.getProgress());
+
+            }
+
+
         }
 
     }
 
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
-
+        musicPlayer.pause();
     }
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-
+        musicPlayer.start();
     }
 }
